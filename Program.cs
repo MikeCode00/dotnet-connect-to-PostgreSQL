18dd1f417149ed62cfb708120d3275c7e22746d1
MikeCode00 <testsendmailfordev@gmail.com>
2023-08-25 22:11:01 +0800

    psql

---
+++ Program.cs
@@ -0,0 +1,24 @@
+﻿
+using System.Data;
+using Npgsql;
+
+IDbConnection ConnectDB() {
+  return new NpgsqlConnection("Host=localhost;Username=mike;Password=password;Port=5432;Database=test");
+}
+
+void OpenDb() {
+  var conn = ConnectDB();
+  try
+  {
+    conn.Open();
+    Console.WriteLine("Database connection is on");
+    conn.Close();
+  }
+  catch (System.Exception e)
+  {
+    Console.WriteLine(e.Message);
+    throw;
+  }
+}
+
+OpenDb();
